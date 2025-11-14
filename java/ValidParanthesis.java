bde2959bc139105d27c74ea4df3a1394d58756e5
Meghana Adithi Bongu <meghanaadithi0@gmail.com>
2025-11-13 19:48:28 -0500

    Valid Paranthesis

---
+++ java/ValidParanthesis.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if(c == '(') stack.push(')');
+            else if(c == '[') stack.push(']');
+            else if(c == '{') stack.push('}');
+            else{
+               
+                if (stack.isEmpty()) return false;
+                
+                char top = stack.pop();
+                if (top != c) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
